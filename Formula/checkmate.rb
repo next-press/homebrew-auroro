47e4c5bd377a26466c9b023ee7c2922799ffc8d9
Arindo Duque <aanduque@users.noreply.github.com>
2026-02-25 08:42:48 +0100

    checkmate 0.1.0: use private repo download strategy

--- Formula/checkmate.rb
+++ Formula/checkmate.rb
@@ -6,7 +6,7 @@ class Checkmate < Formula
 
   depends_on arch: :arm64
 
-  url "https://github.com/next-press/auroro/releases/download/checkmate-v0.1.0/checkmate-0.1.0-darwin-arm64.tar.gz"
+  url "https://github.com/next-press/auroro/releases/download/checkmate-v0.1.0/checkmate-0.1.0-darwin-arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "b0c93ffdc0853092462df117805b04b8916e1ada73dcdcfee45fac5e27aef929"
 
   def install
